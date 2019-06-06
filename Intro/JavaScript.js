6b509d63d13b2d0995b4f494551473d52f0be569
hansi <aaslandhans@gmail.com>
2019-06-06 15:53:26 +0200

    voteknapper og font

--- Intro/JavaScript.js
+++ Intro/JavaScript.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 
-$('.thumbup').on('click', function() {
-    
+$('.thumbup',).on('click', function() {
+    var cnt=0;
+    var btn = $(this);
+    btn.button('loading');
+    setTimeout(function () {
+        cnt++;
+        btn.button('reset');
+        btn.text('  ' + cnt);
+    });
+});
+
+$('.thumbdown',).on('click', function() {
     var cnt=0;
     var btn = $(this);
     btn.button('loading');
