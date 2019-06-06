9aeeddb3601a09fcbb51682c1320bad8c4474f1a
Elise <elise.f.s@hotmail.com>
2019-06-06 13:31:51 +0200

    Vote Box Implemented

--- Intro/Dash.js
+++ Intro/Dash.js
@@ -13,9 +13,35 @@ window.onload = function dateMonth() {
 
     document.getElementById("monthBack").innerHTML = month[dateMonth.getMonth()];
     
-    
-    
-    
-    
-    
 }
+
+ /* Hans' Vote Box*/
+
+var votedDown = document.getElementById("votedDown");
+var votedUp = document.getElementById("votedUp");
+
+var downVote = document.getElementById("thumbsDown");
+var upVote = document.getElementById("thumbsUp");
+
+var countDown = 0;
+var countUp = 0;
+
+votedDown.onclick = function() {
+    if (countDown < 5) { 
+        countDown += 1;
+        votedDown.innerHTML = "downvotes: " + countDown;
+   } else {
+        votedDown.innerHTML = "downvotes: 5";
+    }
+}
+
+votedUp.onclick = function() {
+  if (countUp < 5) { 
+        countUp += 1;
+        votedUp.innerHTML = "<br>" + "upvotes: " + countUp;
+    } else {
+        votedUp.innerHTML = "<br>" + "upvotes: 5";
+    }
+}
+
+ /* End Hans' Vote Box*/
