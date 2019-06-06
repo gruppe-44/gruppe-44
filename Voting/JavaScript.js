5ce5397bfeff5a25dec21083e4330172f8d778b9
hansi <aaslandhans@gmail.com>
2019-06-06 20:23:21 +0200

    endring (?)

--- Voting/JavaScript.js
+++ Voting/JavaScript.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 $('.like','.dislike').on('click', function() {
     $('.active').removeClass('active');
     $(this).addClass('active');
-    var cnt=2;
+    var cnt=0;
     var btn = $(this);
     btn.button('loading');
     setTimeout(function () {
